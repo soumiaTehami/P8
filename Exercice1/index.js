14271e6cdc54078483462dde3d74b8433c6f6b35
tehami soumia <sou_mia_46@hotmail.fr>
2024-06-26 21:16:36 +0200

    ex1 et ex2

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,15 @@
 //votre code ici
 
-export default pairNumbers
+export default function pairNumbers(min, max) {
+    let result = [];
+    for (let i = min; i <= max; i++) {
+        if (i % 2 === 0) {
+            result.push(i);
+        }
+    }
+    return result.join(',');
+}
+
+// Exemple d'utilisation
+console.log(pairNumbers(1, 10));  // Affichera "2,4,6,8,10"
+
--- Exercice2/index.js
+++ Exercice2/index.js
@@ -1,5 +1,23 @@
 // Créez votre fonction ici
 
+export default function calculate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            if (num2 === 0) {
+                return "Division by zero is not allowed";
+            }
+            return num1 / num2;
+        default:
+            return "Invalid operator";
+    }
+}
+
 // Exemples d'utilisation de la fonction
 console.log(calculate(5, 3, '+'));   // Affiche 8
 console.log(calculate(10, 4, '-'));  // Affiche 6
@@ -7,5 +25,3 @@ console.log(calculate(7, 2, '*'));   // Affiche 14
 console.log(calculate(12, 3, '/'));  // Affiche 4
 console.log(calculate(8, 0, '/'));   // Affiche "Division by zero is not allowed"
 console.log(calculate(4, 5, '%'));   // Affiche "Invalid operator"
-
-export default calculate
